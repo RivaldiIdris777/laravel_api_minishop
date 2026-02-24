6fed40c0fce16097d691d398d50c6c0ba16f1310
Rivaldiidris777 <flyingriders905@gmail.com>
2026-02-25 00:12:09 +0700

    Perbaikan docker-compose dan route web

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/metrics', function () {
+    $registry = new \Prometheus\CollectorRegistry(
+        new \Prometheus\Storage\InMemory()
+    );
+
+    $counter = $registry->getOrRegisterCounter(
+        'app',
+        'http_requests_total',
+        'Total HTTP Requests',
+        ['method']
+    );
+
+    $counter->inc([request()->method()]);
+
+    $renderer = new \Prometheus\RenderTextFormat();
+    return response(
+        $renderer->render($registry->getMetricFamilySamples()),
+        200
+    )->header('Content-Type', \Prometheus\RenderTextFormat::MIME_TYPE);
+});
